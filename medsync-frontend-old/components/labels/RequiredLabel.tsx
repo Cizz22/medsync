4023a682bae5fccd264da45b2426a7324562aaf2
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-05-03 08:52:17 +0700

    delete old frontend

--- medsync-frontend-old/components/labels/RequiredLabel.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react';
-import { Label } from '../ui/label';
-
-interface Props {}
-
-export default function RequiredLabel(props: Props): ReactElement {
-  const {} = props;
-  return <Label className="text-red-400">* </Label>;
-}
