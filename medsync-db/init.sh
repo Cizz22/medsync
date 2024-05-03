0a27efddb2feedb1aefe5d4e5a940ceae37fe626
Aulia Cisatra <63190964+Cizz22@users.noreply.github.com>
2024-05-03 10:21:58 +0700

    fix

--- medsync-db/init.sh
+++ medsync-db/init.sh
@@ -17,6 +17,12 @@ if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
+
+		# id db is neosync, create keyloak schema
+		if [ "$db" == "neosync" ]; then
+			psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+				CREATE SCHEMA IF NOT EXISTS keycloak;
 	done
 	echo "Multiple databases created"
 fi
+
